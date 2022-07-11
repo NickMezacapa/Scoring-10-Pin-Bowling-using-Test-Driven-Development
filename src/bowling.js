4a2dc1d6a0b9d34884fb7111be439625b66f9def
Nick Mezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-07-10 20:56:03 -0700

    refactor

--- src/bowling.js
+++ src/bowling.js
@@ -6,7 +6,6 @@ const calculateScore = (rolls) => {
         // get the first and second roll for each frame
         const firstRoll = rolls[frameIndex];
         if (firstRoll === 10) {
-            // strike
             // if user strikes, they are awarded 10 points plus the total of their next two throws
             score += 10 + rolls[frameIndex +1] + rolls[frameIndex +2];
             frameIndex++;
